85c3583d22800a1e04313960ea336d33cf8e1dad
Chanwoongs <tkfkdcksdnd@daum.net>
2023-08-28 03:25:44 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string str, temp;
+
+int main()
+{
+    cin >> str;
+    temp = str;
+    reverse(temp.begin(), temp.end());
+    
+    if (str == temp) cout << '1';
+    else cout << '0';
+}
