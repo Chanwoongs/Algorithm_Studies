9e3a8ca6d4d97416183b90416bda79536234b73f
Chanwoongs <tkfkdcksdnd@daum.net>
2023-08-30 22:13:31 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int n;
+
+int main()
+{
+    while(scanf("%d", &n) != EOF)
+    {
+        long long cnt = 1;
+        long long ret = 1;
+        while (1)
+        {
+            if (cnt % n == 0)
+            {
+                printf("%lld\n", ret);
+                break;
+            }
+            else
+            {
+                cnt = (cnt * 10 + 1);
+                cnt = cnt % n;
+                ret++;            
+            }
+        }
+    }
+        
+}
