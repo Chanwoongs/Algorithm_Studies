4622d44b279644eb10583102ef2555134eb2229e
Chanwoongs <tkfkdcksdnd@daum.net>
2023-09-17 02:23:37 +0900

    [Gold IV] Title: 문자열 폭발, Time: 52 ms, Memory: 8756 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,16 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
 using namespace std;
-string S, T, ret;
-int main() 
+
+string s, b, res;
+stack<char> stk;
+
+int main()
 {
-    cin >> S >> T;
-    for(char a : S){
-        ret += a;
-        if(ret.size() >= T.size() && ret.substr(ret.size() - T.size(), T.size()) == T){
-            ret.erase(ret.end() - T.size(), ret.end()); 
-        }
-    }
-    if(!ret.size())cout << "FRULA" << "\n";
-    else cout << ret << "\n";
-    return 0;
+	cin >> s >> b;
+	for (char a : s)
+	{
+		stk.push(a);
+		if (stk.size() >= b.size() && stk.top() == b[b.size() - 1])
+		{
+			string ss = "";
+			for (char i : b)
+			{
+				ss += stk.top();
+				stk.pop();
+			}
+			reverse(ss.begin(), ss.end());
+			if (b != ss)
+			{
+				for (char c : ss)
+				{
+					stk.push(c);
+				}
+			}
+		}
+	}
+	if (stk.size() == 0) cout << "FRULA" << '\n';
+	else
+	{
+		while (stk.size())
+		{
+			res += stk.top();
+			stk.pop();
+		}
+		reverse(res.begin(), res.end());
+		cout << res << '\n';
+	}
 }
