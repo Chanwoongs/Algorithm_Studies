ef13118cb8ec5677e818d07cfcb5c35eeb9d27c5
Chanwoongs <tkfkdcksdnd@daum.net>
2023-09-21 03:42:17 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 68 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.cc
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.cc
@@ -93,6 +93,10 @@ int go(int here)
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	setMap();
 	for (int i = 0; i < n; i++) cin >> a[i];
 	cout << go(0) << '\n';
