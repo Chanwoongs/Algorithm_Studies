e489a949edeb1c687f003b4fe417b3612c1fc083
Chanwoongs <tkfkdcksdnd@daum.net>
2023-09-22 22:33:41 +0900

    [Silver II] Title: 연속합, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,16 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int n, sum, a, ret = -1001; 
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a);  
+        sum += a; 
+        ret = max(ret, sum); 
+        if(sum < 0) sum = 0;   
+    } 
+    printf("%d\n", ret);
+    return 0;
+}
