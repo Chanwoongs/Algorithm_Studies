a0d18382939cdb305a6ab55465204ab55146442d
Chanwoongs <tkfkdcksdnd@daum.net>
2023-09-23 09:16:02 +0900

    [Silver II] Title: 파닭파닭, Time: 908 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+ll s, c, a[1000004], res, sum, resCnt;
+
+bool check(int mid)
+{
+	ll cnt = 0;
+	for (int i = 0; i < s; i++)
+	{
+		cnt += a[i] / mid;
+	}
+	return cnt >= c; // 모든 파닭을 처리할 수 있는가
+}
+
+int main()
+{
+	cin >> s >> c;
+	for (int i = 0; i < s; i++)
+	{
+		cin >> a[i];
+		sum += a[i];
+	}
+	ll lo = 1, hi = 1e9;
+	while (lo <= hi)
+	{
+		ll mid = (lo + hi) / (1LL * 2);
+		if (check(mid))
+		{
+			lo = mid + 1;
+			res = mid;
+		}
+		else hi = mid - 1;
+	}
+	cout << sum - res * c << '\n';
+}
