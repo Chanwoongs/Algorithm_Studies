bb613f782cfbfe2cdf5a1f07d80f54cc64e5a68d
Chanwoongs <tkfkdcksdnd@daum.net>
2023-10-07 18:48:19 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2656 ms, Memory: 48884 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int T;
+
+int main()
+{
+    cin >> T;
+    while (T--)
+    {
+        int t;
+        cin >> t;
+
+        multiset<int> ms;
+
+        for (int k = 0; k < t; k++)
+        {
+            char a;
+            int num;
+            cin >> a >> num;
+
+            if (a == 'D' && !ms.empty())
+            {
+                if (num == 1)
+                {
+                    ms.erase(--ms.end());
+                }
+                else
+                {
+                    ms.erase(ms.begin());
+                }
+            }
+            else if (a == 'I')
+            {
+                ms.insert(num);
+            }
+        }
+
+        if (ms.empty()) cout << "EMPTY" << '\n';
+        else
+        {
+            cout << *(--ms.end()) << ' ' << *ms.begin() << '\n';
+        }
+    }
+}
