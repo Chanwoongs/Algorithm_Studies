e9cecbc2d0e78cdb8a999fe6beafb56a2d4e0c24
Chanwoongs <tkfkdcksdnd@daum.net>
2023-10-11 13:35:15 +0900

    [Gold V] Title: 이진 검색 트리, Time: 44 ms, Memory: 2500 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int num;
+vector<int> v;
+
+void go(int s, int e)
+{
+    if (s >= e) return;
+    if (s == e - 1)
+    {
+        cout << v[s] << '\n';
+        return;
+    }
+    
+    int next = s + 1;
+    while (next < v.size())
+    {
+        if (v[s] < v[next]) break;
+        next++;
+    }
+    
+    go (s + 1, next);
+    go (next, e);
+    cout << v[s] << '\n';
+}
+
+int main()
+{
+    while (cin >> num)
+    {
+        v.push_back(num);
+    }
+    go(0, v.size());
+}
