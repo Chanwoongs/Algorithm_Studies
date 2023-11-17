150e7d00e39d1a10da0b935e0b578afdb3b29337
Chanwoong Bang <tkfkdcksdnd@daum.net>
2023-11-17 15:49:04 +0900

    [Gold V] Title: 보물섬, Time: 132 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,41 +1,40 @@
 #include <iostream>
-#include <string.h>
 #include <queue>
+#include <tuple>
+
 using namespace std;
 
-char a[54][54];
-int visited[54][54];
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
 
-int n, m, mx;
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, 1, 0, -1 };
+int a[54][54], visited[54][54];
+char c;
+int n, m, res = -1;
 
 void BFS(int y, int x)
 {
-	memset(visited, 0, sizeof(visited));
+	fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
 	visited[y][x] = 1;
 
-	queue<pair<int,int>> q;
+	queue<pair<int, int>> q; 
 	q.push({ y, x });
 
 	while (q.size())
 	{
-		pair<int, int> here = q.front();
-		q.pop();
-
+		tie(y, x) = q.front(); q.pop();
+	
 		for (int i = 0; i < 4; i++)
 		{
-			int ny = here.first + dy[i];
-			int nx = here.second + dx[i];
+			int ny = y + dy[i];
+			int nx = x + dx[i];
 
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-			if (visited[ny][nx]) continue;
-			if (a[ny][nx] == 'L')
-			{
-				visited[ny][nx] = visited[here.first][here.second] + 1;
-				q.push({ ny, nx });
-				mx = max(mx, visited[ny][nx]);
-			}
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx])
+				continue;
+			if (a[ny][nx] == 1) continue;
+			
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ ny, nx });
+			res = max(res, visited[ny][nx]);
 		}
 	}
 }
@@ -46,9 +45,12 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
+		string s;
+		cin >> s;
 		for (int j = 0; j < m; j++)
 		{
-			cin >> a[i][j];
+			if (s[j] == 'W') a[i][j] = 1;
+			else a[i][j] = 0;
 		}
 	}
 
@@ -56,8 +58,12 @@ int main()
 	{
 		for (int j = 0; j < m; j++)
 		{
-			if (a[i][j] == 'L') BFS(i, j);
+			if (a[i][j] == 0)
+			{
+				BFS(i, j);
+			}
 		}
 	}
-	cout << mx - 1 << '\n';
+
+	cout << res - 1 << '\n';
 }
