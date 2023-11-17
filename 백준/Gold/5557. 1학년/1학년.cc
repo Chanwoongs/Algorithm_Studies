f043b4dd49676193f31fb6ab03851015979a8bc7
Chanwoong Bang <tkfkdcksdnd@daum.net>
2023-11-17 21:48:03 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -1,24 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-typedef long long ll;
-ll n, a[104], dp[104][21];
-ll go(int idx, int sum){
-    if(sum < 0 ||sum >20) return 0;
-    ll &ret = dp[idx][sum];
-    if(ret) return ret;
-    if(idx == n - 2){
-        if(sum == a[n - 1]) return 1;
-        return 0;
-    } 
-    ret += go(idx + 1, sum + a[idx + 1]);
-    ret += go(idx + 1, sum - a[idx + 1]);
-    return ret;
-}
-int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    } 
-    cout << go(0, a[0]) << "\n";
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> nums(N);
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+
+    vector<vector<long long>> dp(N, vector<long long>(21, 0));
+    dp[0][nums[0]] = 1;
+
+    for (int i = 1; i < N - 1; i++) {
+        for (int j = 0; j <= 20; j++) {
+            if (j + nums[i] <= 20) {
+                dp[i][j + nums[i]] += dp[i - 1][j];
+            }
+            if (j - nums[i] >= 0) {
+                dp[i][j - nums[i]] += dp[i - 1][j];
+            }
+        }
+    }
+
+    cout << dp[N - 2][nums[N - 1]] << endl;
     return 0;
 }
