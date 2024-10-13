f20fc54e05c24866ddfff851f669f11da7abb35e
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-10-13 21:11:40 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241013/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, prod = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        prod *= a;
+    }
+
+    cout << prod;
+    return 0;
+}
