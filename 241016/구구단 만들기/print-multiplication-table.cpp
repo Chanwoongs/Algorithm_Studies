df54bf868f4fffcc4e2c3c19d3951f7a1c67d690
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-10-16 14:46:31 +0900

    Title: 구구단 만들기, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    if (b % 2 != 0)
+    {
+        b -= 1;
+    }
+
+    for (int i = 0; i < 9; i ++)
+    {
+        for (int j = b; j >= a; j -= 2)
+        {
+            cout << j << " * " << i + 1 << " = " << j * (i + 1) << ' ';
+
+            if (j != 2)
+            {
+                cout << "/ ";
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
