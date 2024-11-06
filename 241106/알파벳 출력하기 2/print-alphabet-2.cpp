5f0282fd288e6f2b8df34614dd02aa7362e89926
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-11-06 17:01:27 +0900

    Title: 알파벳 출력하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241106/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = (int)'A';
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > j)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << (char)a++;
+            }
+            if (a == (int)'Z' + 1) a = (int)'A';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
