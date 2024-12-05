846e1aab32321666d9472fac1a94083c5fd080f8
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-12-05 14:33:24 +0900

    Title: ab곱 n번 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241205/ab곱 n번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {   
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, res = 1;
+        cin >> a >> b;
+
+        for (int i = a; i <= b; i++)
+        {
+            res *= i;
+        }
+
+        cout << sum << '\n';
+    }
+
+    return 0;
+}
