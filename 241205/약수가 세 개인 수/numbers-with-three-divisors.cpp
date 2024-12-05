30efb1c66811541e2de629fa456509e72a9d0d11
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-12-05 15:03:25 +0900

    Title: 약수가 세 개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241205/약수가 세 개인 수/numbers-with-three-divisors.cpp
+++ 241205/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -9,7 +9,7 @@ int main() {
     for (int i = st; i <= ed; i++)
     {
         int cnt = 0;
-        for (int j = 1; j < i; j++)
+        for (int j = 1; j <= i; j++)
         {
             if (i % j == 0)
             {
@@ -18,7 +18,6 @@ int main() {
         }
         if (cnt == 3)
         {
-            cout << j << ' ';
             res++;
         }
     }
